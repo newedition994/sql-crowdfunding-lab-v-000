0695b284563e3328c8b18e5f978e825378ec3d7c
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-20 21:12:06 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,13 +4,13 @@ CREATE TABLE projects (
     category TEXT,
     funding_goal INTEGER,
     start_date INTEGER,
-    end_date INTEGER,
+    end_date INTEGER
 );
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
     name TEXT,
-    age INTEGER,
+    age INTEGER
 );
 
 CREATE TABLE pledge (
