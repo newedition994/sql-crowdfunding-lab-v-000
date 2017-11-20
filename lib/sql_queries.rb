3c9e56101e1a85ed0180c001c4c1eb801b62dda2
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-20 21:46:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,10 +35,7 @@ GROUP BY users.name ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, pledges.amount FROM projects
-INNER JOIN pledges
-ON projects.id = pledges.user_id
-WHERE projects.category = 'music'"
+  "SELECT projects.category, pledges.amount FROM projects INNER JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
